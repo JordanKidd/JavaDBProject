aeade31cd24cb47e7a9f6411cbfbe6029827d4a3
Jordan <jordan.m.kidd@wmich.edu>
2014-11-04 15:38:54 -0500

    added jdbc jar and example select statement

--- AS4-DBMS/src/as4/dbms/AS4DBMS.java
+++ AS4-DBMS/src/as4/dbms/AS4DBMS.java
@@ -1,11 +1,48 @@
 package as4.dbms;
 
-public class AS4DBMS {
+import java.sql.*;
 
-    public static void main(String[] args) {
+public class AS4DBMS {
+    
+    static final String DB_URL = "jdbc:mysql://localhost/lab2";
+
+    public static void main(String[] args) throws SQLException {
         
-        System.out.println("Hello world!!");
+        //Database:
+        String user = "jordan";
+        String password = "";
         
+        Connection conn = DriverManager.getConnection(DB_URL, user, password);
+        
+        String sql = "select * from customers;";
+        try {
+            Statement stmt = conn.createStatement();    //Connection to statement
+            ResultSet rs = stmt.executeQuery(sql);      //exec statement -> table (rs)
+            ResultSetMetaData meta = rs.getMetaData();
+            int colCount = meta.getColumnCount();       //Column count 
+            String row = "";
+            try  {
+                for(int i = 1; i < colCount+1; i++) {
+                    row += String.format("%-26s", meta.getColumnName(i));
+                }
+                row = "";       //RESET LINE
+                
+                while (rs.next()) {
+                    for(int i = 1; i < colCount+1; i++) {
+                        row += String.format("%-26s", rs.getString(i));
+                    }
+                    row = "";       //RESET LINE
+                }
+            } finally {
+              rs.close();
+            }
+            System.out.println();
+            stmt.close();    
+        }
+        catch (SQLException ex) {
+            System.out.println("ERROR ON SELECT STATEMENT!");
+            System.out.println(ex.toString());
+        }
     }
     
 }
