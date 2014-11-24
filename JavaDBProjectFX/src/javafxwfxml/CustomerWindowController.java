0108ad7eda16dd76cd4dee1875b5c9dcd561020b
Jordan Kidd <jordankidd14@gmail.com>
2014-11-23 22:09:50 -0500

    UI for customer searches

--- JavaDBProjectFX/src/javafxwfxml/CustomerWindowController.java
+++ JavaDBProjectFX/src/javafxwfxml/CustomerWindowController.java
@@ -9,7 +9,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -23,9 +27,87 @@ public class CustomerWindowController implements Initializable {
     @FXML
     private MenuItem quitMenuItem;
     
+    @FXML
+    private ComboBox genreComboBox;
+    private String currentGenre;
+    
+    @FXML
+    private ComboBox platformComboBox;
+    private String currentPlatform;
+    
+    @FXML
+    private Button searchButton;
+    
+    @FXML
+    private Button resetButton;
+    
+    @FXML
+    private TextField titleTextField;
+    
+    @FXML
+    private Slider minPrice;
+    
+    @FXML
+    private Slider maxPrice;
+    
+    
+    //----------------------------------------------------------------
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //TODO
+        genreComboBox.getItems().addAll(
+                "", //FOR NO SPECIFC GENRE
+                "Action",
+                "Adventure",
+                "Arcade",
+                "Educational",
+                "Indie",
+                "Puzzle",
+                "Role-Playing",
+                "MMORPG",
+                "Simulations",
+                "Sports",
+                "Strategy"
+        );
+        
+        platformComboBox.getItems().addAll(
+                "", //FOR NO SPECIFC GENRE
+                "Super Nintendo",
+                "Gamecube",
+                "Wii",
+                "Wii U",
+                "Playstation",
+                "Playstation 2",
+                "Playstation 3",
+                "Playstation 4",
+                "Xbox",
+                "Xbox 360",
+                "Xbox One"
+        );
+    }
+    
+    @FXML
+    public void updateCurrentGenreChoice() {
+        Object val = genreComboBox.getValue();
+        currentGenre = val.toString();
+    }
+    
+    @FXML
+    public void updateCurrentPlatformChoice() {
+        Object val = platformComboBox.getValue();
+        currentPlatform = val.toString();
+    }
+    
+    @FXML
+    public void searchButtonClick() {
+        System.out.println("Search clicked!");
+    }
+    
+    @FXML
+    public void resetButtonClick() {
+        titleTextField.setText("");
+        genreComboBox.setValue("");
+        platformComboBox.setValue("");
     }
     
     @FXML
